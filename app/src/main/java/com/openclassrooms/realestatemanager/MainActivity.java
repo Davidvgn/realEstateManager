28eeeb6d9c21aac1c15b5a02b3582aed34a3c7ae
DavidVgn <58291132+Davidvgn@users.noreply.github.com>
2023-04-27 17:39:04 +0200

    First corrections asked

--- app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.java
@@ -5,6 +5,8 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView textViewMain;
@@ -15,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textViewMain = findViewById(R.id.activity_second_activity_text_view_main);
+        textViewMain = findViewById(R.id.activity_main_activity_text_view_main);
         textViewQuantity = findViewById(R.id.activity_main_activity_text_view_quantity);
 
         configureTextViewMain();
@@ -30,6 +32,6 @@ public class MainActivity extends AppCompatActivity {
     private void configureTextViewQuantity(){
         int quantity = Utils.convertDollarToEuro(100);
         textViewQuantity.setTextSize(20);
-        textViewQuantity.setText(quantity);
+        textViewQuantity.setText(String.format(Locale.getDefault(), "%d", quantity));
     }
 }
