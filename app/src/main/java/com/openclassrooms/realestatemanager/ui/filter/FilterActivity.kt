02d2856280703ff3643ca05676360506fa395689
David Vignon <davidvignon1986@gmail.com>
2024-02-17 11:38:52 +0100

    create filter xml

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/filter/FilterActivity.kt
@@ -0,0 +1,8 @@
+package com.openclassrooms.realestatemanager.ui.filter
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class FilterActivity: AppCompatActivity() {
+}
