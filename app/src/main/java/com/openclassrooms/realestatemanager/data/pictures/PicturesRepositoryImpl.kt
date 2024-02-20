b0315a775346379a1c35690faeec99fa653081c8
David Vignon <davidvignon1986@gmail.com>
2024-02-20 08:57:00 +0100

    creating add useCase

--- app/src/main/java/com/openclassrooms/realestatemanager/data/pictures/PicturesRepositoryImpl.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/data/pictures/PicturesRepositoryImpl.kt
@@ -5,6 +5,7 @@ import com.openclassrooms.realestatemanager.domain.pictures.PicturesRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -12,7 +13,13 @@ import javax.inject.Singleton
 class PicturesRepositoryImpl @Inject constructor(
     private val picturesDao: PicturesDao
 ): PicturesRepository {
+
     override fun getPicturesAsFlow(): Flow<List<PicturesEntity>> =
         picturesDao.getPicturesAsFlow().flowOn(Dispatchers.IO)
 
+    override suspend fun addPictures(pictures: List<PicturesEntity>) {
+        withContext(Dispatchers.IO) {
+            picturesDao.insertAll(pictures)
+        }
+    }
 }
--- app/src/main/java/com/openclassrooms/realestatemanager/domain/pictures/PicturesRepository.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/domain/pictures/PicturesRepository.kt
@@ -3,5 +3,9 @@ package com.openclassrooms.realestatemanager.domain.pictures
 import kotlinx.coroutines.flow.Flow
 
 interface PicturesRepository {
+
     fun getPicturesAsFlow(): Flow<List<PicturesEntity>>
+    suspend fun addPictures(pictures: List<PicturesEntity>)
+
+
 }
