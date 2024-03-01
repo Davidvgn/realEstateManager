77fade2aec30bee33326a95deb64ac24292fc4a8
David Vignon <davidvignon1986@gmail.com>
2024-03-01 12:51:31 +0100

    Update ContentProviderTest (bug w/pictures no Update on view in Addform)

--- app/src/test/java/com/openclassrooms/realestatemanager/data/content_provider/ContentProviderTest.kt
+++ app/src/test/java/com/openclassrooms/realestatemanager/data/content_provider/ContentProviderTest.kt
@@ -1,9 +1,7 @@
 package com.openclassrooms.realestatemanager.data.content_provider
 
-import android.content.Context
 import android.database.Cursor
 import android.net.Uri
-import androidx.test.core.app.ApplicationProvider
 import com.openclassrooms.realestatemanager.data.pictures.PicturesDao
 import com.openclassrooms.realestatemanager.data.real_estates.RealEstateDao
 import io.mockk.every
@@ -24,11 +22,9 @@ class ContentProviderTest {
     private lateinit var contentProvider: ContentProvider
     private lateinit var realEstateDaoMock: RealEstateDao
     private lateinit var picturesDaoMock: PicturesDao
-    private lateinit var context: Context
 
     @Before
     fun setup() {
-        context = ApplicationProvider.getApplicationContext()
         realEstateDaoMock = mockk()
         picturesDaoMock = mockk()
 
@@ -39,10 +35,10 @@ class ContentProviderTest {
         }
 
         val fakeCursor: Cursor = mockk()
-        every { realEstateDaoMock.getAllPropertiesWithCursor() } returns fakeCursor
         justRun {
             fakeCursor.setNotificationUri(any(), any())
         }
+        every { realEstateDaoMock.getAllPropertiesWithCursor() } returns fakeCursor
     }
 
     @Test
